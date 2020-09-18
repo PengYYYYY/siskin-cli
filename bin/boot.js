9b3bfa30110a5b0a95f6d6f24608bb468d6bfdf3
yue.peng <yue.peng@ly.com>
2020-09-18 22:55:04 +0800

    feat(add mutil-color log): add mutil-color log

--- bin/boot.js
+++ bin/boot.js
@@ -2,10 +2,19 @@
 const program = require('commander')
 program.version(require('../package.json').version)
 
-
 program
   .command('init <name>')
   .description('init project')
   .action(require('../lib/init'))
+
+program
+  .command('refresh')
+  .description('monitoring vue2.0 adn vue3.0')
+  .action(require('../lib/refresh'))
+
+program
+  .command('doctor')
+  .description('monitoring vue2.0 adn vue3.0')
+  .action(require('../lib/doctor'))
 
 program.parse(process.argv)
